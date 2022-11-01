2778a5bb5818630ac0494f9594ca8be92572ecb1
Manuel Munz <munz@comuno.net>
2022-11-01 09:08:18 +0100

    Fix page TSConfig registration

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,10 +4,10 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('c1_fsc_video', 'Configuration/TypoScript', 'FSC video content element');
-    // Add page.ts
-    $pageTsConfig = @file_get_contents(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('c1_fsc_video') . 'Configuration/TSConfig/page.ts');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
-});
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'c1_fsc_video',
+    'Configuration/TypoScript',
+    'FSC video content element'
+);
+
 
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,23 +4,26 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-// Register Icon
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:c1_fsc_video/Configuration/TSConfig/page.tsconfig">'
+);
+
+call_user_func(function () {
+    // Register Icon
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
         'c1_fsc_video', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
             'source' => 'EXT:c1_fsc_video/Resources/Public/Icons/mimetypes-media-video.svg'
         )
-);
+    );
+    // register custom render handlers
+    /** @var \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry $rendererRegistry */
+    $rendererRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class
+    );
+    $rendererRegistry->registerRendererClass(C1\C1FscVideo\Rendering\VimeoRenderer::class);
+});
 
 // Register custom youtube/vimeo helpers to get better previews
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['youtube'] = C1\C1FscVideo\Helpers\YouTubeHelper::class;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['vimeo'] = C1\C1FscVideo\Helpers\VimeoHelper::class;
-
-// register custom render handlers
-/** @var \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry $rendererRegistry */
-$rendererRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class
-);
-$rendererRegistry->registerRendererClass(C1\C1FscVideo\Rendering\VimeoRenderer::class);
-unset($rendererRegistry);
-?>
